da61ca2251c06063ba6452a1ff7b60ca31b09864
Unais-12 <unaisbinfaheem16@gmail.com>
2024-09-18 17:18:21 +0500

    New changes

--- app.py
+++ app.py
@@ -3,20 +3,14 @@ from flask_session import Session
 import sqlite3
 import pyodbc
 import sys
-from app import app as application
 import os
 
-path = '/home/Unais1/mysite/flask_app.py'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.chdir(path)
 app = Flask(__name__)
 
 app.config["SESSION_TYPE"] = 'filesystem'
 app.config["SESSION_PERMANENT"] = 'False'
 Session(app)
 
-@app.route("/main", method = ["GET"])
+@app.route("/main", methods = ["GET"])
 def main():
     return "Hello"
