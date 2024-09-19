88d7073129ca4af40bf053de26bb6ca001c6bd36
Unais-12 <unaisbinfaheem16@gmail.com>
2024-09-19 11:14:02 +0500

    Basic Backend implementation

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, redirect, render_template, request, session, jsonify
 from flask_session import Session
 import sqlite3
 import pyodbc
-import sys
 import os
+import json
+import logging
+import requests
+import openai
 
 app = Flask(__name__)
 
@@ -11,6 +14,75 @@ app.config["SESSION_TYPE"] = 'filesystem'
 app.config["SESSION_PERMANENT"] = 'False'
 Session(app)
 
-@app.route("/main", methods = ["GET"])
-def main():
-    return "Hello"
+OPEN_AI_MODEL = "gpt-3.5-turbo"
+MAX_TOKENS = 100
+def get_chat_message_exact(query):
+    return [
+        {
+            "role": "user",
+            "content": query
+        }
+    ]
+
+def get_chat_message(query, previous_chat):
+    messages = []
+
+    if previous_chat:
+        for message in previous_chat:
+            messages.append({
+                "role": message['role'],
+                "content": message['content']
+            })
+
+    messages.append({
+        "role": "user",
+        "content": query
+    })
+
+    return messages
+
+
+
+OPEN_API_KEY = os.getenv("OPENAI_API_KEY")
+OPEN_API_URL = "https://api.openai.com/v1/chat/completions"
+
+@app.route("/openai-response", methods = ["POST"])
+def get_openai_response():
+    data = request.json
+
+    query = data.get('query')
+    previous_chat = data.get('previous_chat')
+    exact = data.get('exact')
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPEN_API_KEY}"
+    }
+    request_body = {
+        "model": OPEN_AI_MODEL,
+        "max_tokens": MAX_TOKENS,
+        "temperature": 1,
+        "messages": get_chat_message_exact(query) if exact else get_chat_message(query, previous_chat)
+    }
+
+    response = requests.post(OPEN_API_URL, headers=headers, data=json.dumps(request_body))
+
+    return jsonify(response.json())
+
+def get_chat_message(query, previous_chat):
+    logging.info(f"QUERY: {query}, PreviousChat: {previous_chat}")
+    
+    # Messages formatted as a list of dictionaries for the OpenAI API
+    messages = [
+        {
+            "role": "user",
+            "content": query
+        },
+        {
+            "role": "assistant",
+            "content": f"Generate a Mermaid code for the requested diagram and only return the code {query} "
+                       f"without any explanations or anything. Please keep in mind this previous conversation: '{previous_chat}'"
+        }
+    ]
+    
+    return messages
